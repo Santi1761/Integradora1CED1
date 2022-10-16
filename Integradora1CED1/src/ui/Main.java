61cb3d2bfca662884e64d84544a2594df9b6b2f7
Santiago Arboleda Velasco <santiagoarboledavelasco@gmail.com>
2022-10-16 14:54:36 -0500

    Menu ready for implementation

--- Integradora1CED1/src/ui/Main.java
+++ Integradora1CED1/src/ui/Main.java
@@ -1,15 +1,14 @@
 package ui;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
 	private Scanner sc;
 	
+	
 	public Main() {
-		this.sc = new Scanner(System.in);
-		
+		this.sc = new Scanner(System.in);		
 	}
 	
 	public static void main(String[] args) {
@@ -18,6 +17,40 @@ public class Main {
 	}
 	
 	public void menu() {
+		System.out.println("////////////////////////////"+
+									"\n	WELCOME"+
+						"\n ///////////////////////// ");
 		
+		System.out.println("1) Register a person"+
+						"\n 2) Show people"+
+						"\n 3) Make an entry"+
+						"\n 4) Make an exit"+
+						"\n 5) Undo entry or exit"+
+						"\n 6) CLOSE PROGRAM");
+		int option = sc.nextInt();
+		
+		switch(option) {
+			case 1:
+				
+				break;
+			case 2:
+				
+				break;
+			case 3:
+				
+				break;
+			case 4:
+				
+				break;
+			case 5:
+				
+				break;
+			case 6:
+				
+				break;
+			default:
+				System.out.print("WRITE A VALID NUMBER");
+		}
+
 	}
 }
